9f856ee264be40d6a3063376c6f97578f06b6d8e
WIN-H2P195CFVTG\Administrator <w15527294190@gmail>
2023-03-11 12:26:11 +0800

    优化依赖项

--- src/hooks/useAsync.ts
+++ src/hooks/useAsync.ts
@@ -1,13 +1,15 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useState, DependencyList, useRef } from 'react';
 
 type AsyncFunction<P = unknown, T = unknown> = (data?: P) => Promise<T>;
 type executeFn<P = unknown> = (data?: P) => void;
 type Status = 'idle' | 'success' | 'pending' | 'error';
 
-const useAsync = <T = unknown, P = unknown>(asyncFunction: AsyncFunction<P, T>, immediate = true) => {
+const useAsync = <T = unknown, P = unknown>(asyncFunction: AsyncFunction<P, T>, deps: DependencyList = [], immediate = true) => {
   const [status, setStatus] = useState<Status>('idle');
   const [value, setValue] = useState<T>();
   const [error, setError] = useState<Error | null>(null);
+  const refFn = useRef<typeof asyncFunction>()
+  refFn.current = asyncFunction
 
   const execute = useCallback<executeFn<P>>(
     async data => {
@@ -15,7 +17,7 @@ const useAsync = <T = unknown, P = unknown>(asyncFunction: AsyncFunction<P, T>, 
       setError(null);
 
       try {
-        const res = await asyncFunction(data);
+        const res = await refFn.current(data);
         setValue(res);
         setStatus('success');
       } catch (err) {
@@ -23,7 +25,7 @@ const useAsync = <T = unknown, P = unknown>(asyncFunction: AsyncFunction<P, T>, 
         setStatus('error');
       }
     },
-    [asyncFunction]
+    [...deps]
   );
 
   useEffect(() => {
@@ -32,7 +34,7 @@ const useAsync = <T = unknown, P = unknown>(asyncFunction: AsyncFunction<P, T>, 
     }
   }, [execute, immediate]);
 
-  return { execute, status, value, error };
+  return { execute, status, loading: status === 'pending', value, error };
 };
 
 export default useAsync;
